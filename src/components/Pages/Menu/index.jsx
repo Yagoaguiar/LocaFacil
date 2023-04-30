48450d2e1d1aa01a7bfccd29899028fcf2e885d2
Yago  Aguiar <yagoaguiar@MacBook-Pro-de-Yago.local>
2023-04-30 04:39:22 -0300

    versão 0.0.2

--- src/components/Pages/Menu/index.jsx
+++ src/components/Pages/Menu/index.jsx
@@ -1,36 +1,34 @@
 import { Link } from "react-router-dom"
 import styles from './Menu.module.css';
+
+
 export default function Menu() {
-
-
-
-
-return (
-
-        <div className={styles.container}>
-            <header className={styles.header}>
-                <ul className={styles.navList}>
-                    <li className={styles.headerBtnGroup}>
-                        <Link to="/Home">Home</Link>
-                    </li>
-                     <li className={styles.headerBtnGroup}>
-                        <Link to="/contato">Contato</Link>
-                    </li>
-                     <li className={styles.headerBtnGroup}>
-                        <Link to="/TodasAsMaquinas">Todas as Máquinas</Link>
-                    </li>
-                     <li className={styles.headerBtnGroup}>
-                        <Link to="/login">Login</Link>
-                    </li>
-                     <li className={styles.headerBtnGroup}>
-                        <Link to="/AdicionarMaquina">AdicionarPorra</Link>
-                    </li>
-                    <li className={styles.headerBtnGroup}>
-                        <Link to="/Card">Card</Link>
-                    </li>
-                </ul>
-            </header>
-        </div>
-    )
-
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <ul className={styles.navList}>
+          <li className={styles.navBtn}>
+            <Link to="/Home">Home</Link>
+          </li>
+          <li className={styles.navBtn}>
+            <Link to="/contato">Contato</Link>
+          </li>
+          <li className={styles.navBtn}>
+            <Link to="/TodasAsMaquinas">Todas as Máquinas</Link>
+          </li>
+          <li className={styles.navBtn}>
+            <Link to="/AdicionarMaquina">Anunciar Sua Máquina</Link>
+          </li>
+          <li className={styles.navBtn}>
+            <Link to="/Card">Card</Link>
+          </li>
+            <li className={styles.navBtn}>
+                <Link to="/Login">Login</Link>
+            </li>
+        </ul>
+        <ul>
+        </ul>
+      </header>
+    </div>
+  );
 }
