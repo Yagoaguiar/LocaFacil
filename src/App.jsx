61eeece7ccf73df133c71aa304e54aa6059c9cf5
Yagoaguiar <yagoaguiar0203@gmail.com>
2023-05-23 18:09:19 -0300

    Não aparece a página home #60

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState } from 'react';
+import { useState } from "react";
 import Home from "../src/components/Pages/Home/index";
 import Login from "./components/Pages/Login/Login";
 import Maquina from "./components/Pages/Maquinas/Maquinas";
@@ -10,7 +10,7 @@ import Card from "./components/Pages/Card/Card";
 import TodasAsMaquinas from "./components/Pages/TodasAsMaquinas/TodasAsMaquinas";
 import AdicionarMaquina from "./components/Pages/AdicionarMaquina/AdicionarMaquina";
 import Cadastro from "./components/Pages/Cadastro/Cadastro";
-import Erro404 from "../src/components/Pages/Erro404/erro"
+import Erro404 from "../src/components/Pages/Erro404/erro";
 
 export default function App() {
   const [logado, setLogado] = useState(false);
@@ -28,25 +28,31 @@ export default function App() {
       <div>
         <Menu />
         <Routes>
-        {logado ? (
-          <>
-            <Route path="/AdicionarMaquina" element={<AdicionarMaquina onLogout={handleLogout}/>} />
-            <Route path="/logout" element={<Login onLogout={handleLogout} />} />
-          </>
-          
-        ) : (
-          <>
-            <Route path="/login" element={<Login onLogin={handleLogin} />} />
-            <Route path="/esquecisenha" element={<EsqueciSenha />} />
-            <Route path="/cadastro" element={<Cadastro />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/TodasAsMaquinas" element={<TodasAsMaquinas />} />
-            <Route path="/maquina" element={<Maquina />} />
-            <Route path="/Card" element={<Card />} />
-            <Route path="/contato" element={<Contato />} />
-          </>
-        )}
-            <Route path="*" element={<Erro404 />} />
+          {logado ? (
+            <>
+              <Route
+                path="/AdicionarMaquina"
+                element={<AdicionarMaquina onLogout={handleLogout} />}
+              />
+              <Route
+                path="/logout"
+                element={<Login onLogout={handleLogout} />}
+              />
+            </>
+          ) : (
+            <>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login onLogin={handleLogin} />} />
+              <Route path="/esquecisenha" element={<EsqueciSenha />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/TodasAsMaquinas" element={<TodasAsMaquinas />} />
+              <Route path="/maquina" element={<Maquina />} />
+              <Route path="/Card" element={<Card />} />
+              <Route path="/contato" element={<Contato />} />
+            </>
+          )}
+          <Route path="*" element={<Erro404 />} />
         </Routes>
       </div>
     </BrowserRouter>
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import Cadastro from './components/Pages/Cadastro'
+import Cadastro from './components/Pages/Cadastro/Cadastro'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
